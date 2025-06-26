bea0d264826224735eeb3c66a08440024da0640c
Likith <youremail@example.com>
2025-06-26 23:11:58 +0530

    Add dynamic port for Render deployment

--- app.py
+++ app.py
@@ -117,4 +117,6 @@ def logout():
     return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))  # Render uses a system-defined PORT
+    app.run(debug=False, host='0.0.0.0', port=port)
+
